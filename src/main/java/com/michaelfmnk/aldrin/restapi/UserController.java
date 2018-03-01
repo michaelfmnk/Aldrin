7aa3d8b46f31dfa7913044376b761216a9a71a26
Michael <michael.fmnk@gmail.com>
2018-03-01 20:04:32 +0200

    not found exception & UserController enhancement

--- src/main/java/com/michaelfmnk/aldrin/restapi/UserController.java
+++ src/main/java/com/michaelfmnk/aldrin/restapi/UserController.java
@@ -1,6 +1,10 @@
 package com.michaelfmnk.aldrin.restapi;
 
+import com.michaelfmnk.aldrin.exception.ResourceNotFoundException;
+import com.michaelfmnk.aldrin.postgres.PostRepository;
 import com.michaelfmnk.aldrin.postgres.UserRepository;
+import com.michaelfmnk.aldrin.postgres.dao.User;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,8 +18,13 @@ public class UserController {
     @Autowired
     UserRepository userRepository;
 
+
     @GetMapping("/{username}")
     public ResponseEntity<?> getUserByUsername(@PathVariable("username") String username){
+        User user = userRepository.findUserByUsername(username);
+        if (user==null){
+            throw new ResourceNotFoundException("User with username " + username + " not found");
+        }
         return ResponseEntity.ok(userRepository.findUserByUsername(username));
     }
 }
